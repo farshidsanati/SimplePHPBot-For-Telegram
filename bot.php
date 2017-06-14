92847f1fe70d3e945bbff7c153dd114193602e79
Farshid Sanati <farshidsanati@gmail.com>
2017-06-14 14:52:51 +0430

    Simple PHP Bot Telegram
    Simple PHP Bot without using library !
    
    Contribution settings

---
+++ bot.php
@@ -0,0 +1,126 @@
+<?php
+
+############################################################################################
+define('API_KEY','BOTTOKEN');
+############################################################################################
+
+//----######------
+function makereq($method,$datas=[]){
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,http_build_query($datas));
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
+//##############=--API_REQ
+function apiRequest($method, $parameters) {
+  if (!is_string($method)) {
+    error_log("Method name must be a string\n");
+    return false;
+  }
+  if (!$parameters) {
+    $parameters = array();
+  } else if (!is_array($parameters)) {
+    error_log("Parameters must be an array\n");
+    return false;
+  }
+  foreach ($parameters as $key => &$val) {
+    // encoding to JSON array parameters, for example reply_markup
+    if (!is_numeric($val) && !is_string($val)) {
+      $val = json_encode($val);
+    }
+  }
+  $url = "https://api.telegram.org/bot".API_KEY."/".$method.'?'.http_build_query($parameters);
+  $handle = curl_init($url);
+  curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
+  curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+  return exec_curl_request($handle);
+}
+//----######------
+//---------
+$update = json_decode(file_get_contents('php://input'));
+var_dump($update);
+//=========
+$chat_id = $update->message->chat->id;
+$message_id = $update->message->message_id;
+$from_id = $update->message->from->id;
+$name = $update->message->from->first_name;
+$username = $update->message->from->username;
+$textmessage = isset($update->message->text)?$update->message->text:'';
+$reply = $update->message->reply_to_message->forward_from->id;
+$stickerid = $update->message->reply_to_message->sticker->file_id;
+$photo = $update->message->photo;
+$video = $update->message->video;
+$sticker = $update->message->sticker;
+$file = $update->message->document;
+$music = $update->message->audio;
+$voice = $update->message->voice;
+$forward = $update->message->forward_from;
+$admin = 66443035;
+//-------
+function SendMessage($ChatId, $TextMsg)
+{
+ makereq('sendMessage',[
+'chat_id'=>$ChatId,
+'text'=>$TextMsg,
+'parse_mode'=>"MarkDown"
+]);
+}
+function SendSticker($ChatId, $sticker_ID)
+{
+ makereq('sendSticker',[
+'chat_id'=>$ChatId,
+'sticker'=>$sticker_ID
+]);
+}
+function Forward($KojaShe,$AzKoja,$KodomMSG)
+{
+makereq('ForwardMessage',[
+'chat_id'=>$KojaShe,
+'from_chat_id'=>$AzKoja,
+'message_id'=>$KodomMSG
+]);
+}
+function save($filename,$TXTdata)
+	{
+	$myfile = fopen($filename, "w") or die("Unable to open file!");
+	fwrite($myfile, "$TXTdata");
+	fclose($myfile);
+	}
+//===========
+ if($textmessage == '/start')
+{
+SendMessage($chat_id,"*Welcome* `#$name` :)\nYou Can Change This ;)");
+}
+elseif ($textmessage == '/fwrd')
+{
+SendMessage($chat_id,"Now I Forward Your Text To You :X");
+Forward($chat_id,$chat_id,$message_id);
+}
+elseif ($textmessage == '/inlinekb')
+{
+var_dump(makereq('sendMessage',[
+        'chat_id'=>$update->message->chat->id,
+        'text'=>"_This Is Simple Inline Keyboard Only For Only for_ *training*",
+	'parse_mode'=>'MarkDown',
+        'reply_markup'=>json_encode([
+            'inline_keyboard'=>[
+                [
+                    ['text'=>"Join SpyGuard Channel ðŸ‘‘",'url'=>"https://telegram.me/SpyGuard"]
+                ]
+            ]
+        ])
+    ]));
+}
+else
+{
+SendMessage($chat_id,"*Command Not Found*");
+}
+?>
